1189bb7dc18ea497631f29f4a83047966d7e2809
Eishieis <justjeed123@gmail.com>
2026-09-16 16:23:08 -0300

    Add project files.

---
+++ MoviesAdministration/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using MoviesAdministration.Models;
+using System.Diagnostics;
+
+namespace MoviesAdministration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Readme()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MoviesAdministration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesAdministration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
